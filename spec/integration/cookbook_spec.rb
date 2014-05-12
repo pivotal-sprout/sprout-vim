4e3af5cc0b42c35486d3debb721e9a394874cb43
David Dening and Grant Hutchins <pair+ddening+grant@pivotallabs.com>
2014-05-12 15:56:40 -0400

    Test that vim, .vim, and tmux get installed

--- spec/integration/cookbook_spec.rb
+++ spec/integration/cookbook_spec.rb
@@ -1,11 +1,21 @@
 require 'spec_helper'
 
 describe 'sprout-vim' do
-  before :all do
-    expect(File).not_to be_exists("#{ENV['HOME']}/vim")
-  end
+  it 'installs MacVim, adds the .vim directory, and installs tmux' do
+    brew_prefix = Bundler.with_clean_env { `brew --prefix`.chomp }
 
-  it 'converges' do
+    mvim_filename = File.join(brew_prefix, 'bin', 'mvim')
+    dotvim_directory = File.expand_path('~/.vim')
+    tmux_filename = File.join(brew_prefix, 'bin', 'tmux')
+
+    expect(File.exist?(mvim_filename)).to be_false, "Please run `brew uninstall macvim`"
+    expect(File.exist?(dotvim_directory)).to be_false, "Please run `rm -rf ~/.vim`"
+    expect(File.exist?(tmux_filename)).to be_false, "Please run `brew uninstall tmux`"
+
     expect(system('soloist')).to be_true
+
+    expect(File.exist?(mvim_filename)).to be_true
+    expect(File.exist?(dotvim_directory)).to be_true
+    expect(File.exist?(tmux_filename)).to be_true
   end
 end
