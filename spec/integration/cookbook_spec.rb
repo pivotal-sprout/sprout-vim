2e3f7b265742e1f27381118f6c96e0bddf15155b
David Dening and Grant Hutchins <pair+ddening+grant@pivotallabs.com>
2014-05-14 12:17:15 -0400

    Prefer single-quoted strings to double-quoted

--- spec/integration/cookbook_spec.rb
+++ spec/integration/cookbook_spec.rb
@@ -8,9 +8,9 @@ describe 'sprout-vim' do
     dotvim_directory = File.expand_path('~/.vim')
     tmux_filename = File.join(brew_prefix, 'bin', 'tmux')
 
-    expect(File.exist?(mvim_filename)).to be_false, "Please run `brew uninstall macvim`"
-    expect(File.exist?(dotvim_directory)).to be_false, "Please run `rm -rf ~/.vim`"
-    expect(File.exist?(tmux_filename)).to be_false, "Please run `brew uninstall tmux`"
+    expect(File.exist?(mvim_filename)).to be_false, 'Please run `brew uninstall macvim`'
+    expect(File.exist?(dotvim_directory)).to be_false, 'Please run `rm -rf ~/.vim`'
+    expect(File.exist?(tmux_filename)).to be_false, 'Please run `brew uninstall tmux`'
 
     expect(system('soloist')).to be_true
 
