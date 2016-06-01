e861e0d6b2ce0737b5988d558801e4ab53c4f03a
Dan Wendorf <dwendorf@pivotal.io>
2016-05-31 19:53:03 -0700

    Remove unused cookbook dependencies

--- metadata.rb
+++ metadata.rb
@@ -7,7 +7,6 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.2.0'
 supports         'mac_os_x'
 depends          'sprout-base'
-depends          'sprout-osx-apps'
 depends          'homebrew'
 
 issues_url 'https://github.com/pivotal-sprout/sprout-vim/issues'
