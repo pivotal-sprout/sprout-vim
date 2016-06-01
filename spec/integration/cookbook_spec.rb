5b4a1f3f95d387286cdc667b88d6de679460a9d1
Dan Wendorf <dwendorf@pivotal.io>
2016-05-31 20:58:40 -0700

    Integration specs don't need brew to be pre-installed

--- spec/integration/cookbook_spec.rb
+++ spec/integration/cookbook_spec.rb
@@ -2,15 +2,20 @@ require 'spec_helper'
 
 RSpec.describe 'sprout-vim' do
   it 'installs MacVim, adds the .vim directory, and installs tmux' do
-    brew_prefix = Bundler.with_clean_env { `brew --prefix`.chomp }
+    brew_installed = system('which brew')
 
-    mvim_filename = File.join(brew_prefix, 'bin', 'mvim')
+    if brew_installed
+      brew_prefix = Bundler.with_clean_env { `brew --prefix`.chomp }
+
+      mvim_filename = File.join(brew_prefix, 'bin', 'mvim')
+      tmux_filename = File.join(brew_prefix, 'bin', 'tmux')
+
+      expect(File.exist?(mvim_filename)).to be_false, 'Please run `brew uninstall macvim`'
+      expect(File.exist?(tmux_filename)).to be_false, 'Please run `brew uninstall tmux`'
+    end
+
     dotvim_directory = File.expand_path('~/.vim')
-    tmux_filename = File.join(brew_prefix, 'bin', 'tmux')
-
-    expect(File.exist?(mvim_filename)).to be_false, 'Please run `brew uninstall macvim`'
     expect(File.exist?(dotvim_directory)).to be_false, 'Please run `rm -rf ~/.vim`'
-    expect(File.exist?(tmux_filename)).to be_false, 'Please run `brew uninstall tmux`'
 
     expect(system('soloist')).to be_true
 
