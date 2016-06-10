5d7a066a9b323f79069bb7995c552b85eba8b9cd
aram price <aprice@pivotal.io>
2016-06-10 16:47:05 -0700

    Add comment to command stub

--- spec/unit/spec_helper.rb
+++ spec/unit/spec_helper.rb
@@ -7,7 +7,7 @@ RSpec.configure do |config|
   config.version = '10.11.1' # via https://github.com/customink/fauxhai/tree/master/lib/fauxhai/platforms/mac_os_x
   config.before do
     stub_const('ENV', 'SUDO_USER' => 'fauxhai')
-    stub_command('which git').and_return(true)
+    stub_command('which git').and_return(true) # because of homebrew recipe
   end
   config.after(:suite) { FileUtils.rm_r('.librarian') }
 end
