da1be61753097fe76d763a284f98644a8431ca4f
aram price <aprice@pivotal.io>
2016-06-10 15:33:41 -0700

    Simplify integration spec

--- spec/integration/cookbook_spec.rb
+++ spec/integration/cookbook_spec.rb
@@ -2,29 +2,20 @@ require 'spec_helper'
 
 RSpec.describe 'sprout-vim' do
   let(:dotvim_directory) { File.expand_path('~/.vim') }
+  let(:brew_installed_mvim) { File.join('/usr/local/bin/mvim') }
+  let(:brew_installed_tmux) { File.join('/usr/local/bin/tmux') }
 
   before do
-    brew_installed = system('which brew')
-
-    if brew_installed
-      brew_prefix = Bundler.with_clean_env { `brew --prefix`.chomp }
-
-      mvim_filename = File.join(brew_prefix, 'bin', 'mvim')
-      tmux_filename = File.join(brew_prefix, 'bin', 'tmux')
-
-      expect(File.exist?(mvim_filename)).to be_false, 'Please run `brew uninstall macvim`'
-      expect(File.exist?(tmux_filename)).to be_false, 'Please run `brew uninstall tmux`'
-    end
-
-    puts `ls -alFG #{dotvim_directory}`
-    expect(system("sudo rm -rf #{dotvim_directory}")).to be_true, "Unable to `rm -rf #{dotvim_directory}`"
+    expect(File).not_to exist?(brew_installed_mvim)
+    expect(File).not_to exist?(brew_installed_tmux)
+    expect(File).not_to exist?(dotvim_directory)
   end
 
   it 'installs MacVim, adds the .vim directory, and installs tmux' do
     expect(system('soloist')).to be_true
 
-    expect(File.exist?(mvim_filename)).to be_true
-    expect(File.exist?(dotvim_directory)).to be_true
-    expect(File.exist?(tmux_filename)).to be_true
+    expect(File).to exist?(brew_installed_mvim)
+    expect(File).to exist?(brew_installed_tmux)
+    expect(File).to exist?(dotvim_directory)
   end
 end
